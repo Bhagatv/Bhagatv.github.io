ed117db436fa50b0202bcf896b72ce88859b45fa
bhagatv <bhagatv@uci.edu>
2019-10-27 10:14:36 -0700

    Updates

---
+++ precache-manifest.037f49f32e763b10a91efab4f7105093.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "796dffcab4ce1425fc8021721edf6165",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "8510a2e567f6cdabafb9",
+    "url": "/react_portfolio/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "172219afa25aa90fabe6",
+    "url": "/react_portfolio/static/css/main.7c77e239.chunk.css"
+  },
+  {
+    "revision": "8510a2e567f6cdabafb9",
+    "url": "/react_portfolio/static/js/2.4e21c643.chunk.js"
+  },
+  {
+    "revision": "172219afa25aa90fabe6",
+    "url": "/react_portfolio/static/js/main.af769592.chunk.js"
+  },
+  {
+    "revision": "2b0f082ecb4643aba4a6",
+    "url": "/react_portfolio/static/js/runtime-main.f8b8a1b2.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/react_portfolio/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5f56d92b5e805c96620dd575992f6667",
+    "url": "/react_portfolio/static/media/veeral_bhagat.5f56d92b.pdf"
+  }
+]);
