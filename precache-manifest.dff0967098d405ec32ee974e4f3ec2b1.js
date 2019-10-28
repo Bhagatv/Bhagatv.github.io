13fb69d7b5f07a9d95d28de235ed0ce5bd3422d9
bhagatv <bhagatv@uci.edu>
2019-10-27 22:00:02 -0700

    Updates

---
+++ precache-manifest.dff0967098d405ec32ee974e4f3ec2b1.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b96cbfe21b16694f488f7945ce029e1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8510a2e567f6cdabafb9",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "becefceb7cb4ad5dd89b",
+    "url": "/static/css/main.e4dc96ca.chunk.css"
+  },
+  {
+    "revision": "8510a2e567f6cdabafb9",
+    "url": "/static/js/2.4e21c643.chunk.js"
+  },
+  {
+    "revision": "becefceb7cb4ad5dd89b",
+    "url": "/static/js/main.cc4760a2.chunk.js"
+  },
+  {
+    "revision": "5ef4829ffb8b2e6bff49",
+    "url": "/static/js/runtime-main.e03af4ce.js"
+  },
+  {
+    "revision": "b14f7d6f928add9e807aca2504c13eac",
+    "url": "/static/media/akuna_logo.b14f7d6f.png"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "5f56d92b5e805c96620dd575992f6667",
+    "url": "/static/media/veeral_bhagat.5f56d92b.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react_portfolio/precache-manifest.5c24a137b25942ffecc31940d10a7f9a.js"
+  "/precache-manifest.dff0967098d405ec32ee974e4f3ec2b1.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react_portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
