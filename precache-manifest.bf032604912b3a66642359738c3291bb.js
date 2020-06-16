aebe85b3cb9f8967e796a61a998e58e23db80dfb
bhagatv <bhagatv@uci.edu>
2020-06-15 20:50:58 -0700

    Updates

---
+++ precache-manifest.bf032604912b3a66642359738c3291bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8be0481f06862b21d6fb9c400f7d62ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b733c356fc487357a081",
+    "url": "/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "72cab1d85109e2b17cd6",
+    "url": "/static/css/main.ba929571.chunk.css"
+  },
+  {
+    "revision": "b733c356fc487357a081",
+    "url": "/static/js/2.41e65f5d.chunk.js"
+  },
+  {
+    "revision": "72cab1d85109e2b17cd6",
+    "url": "/static/js/main.b5304b6b.chunk.js"
+  },
+  {
+    "revision": "5ef4829ffb8b2e6bff49",
+    "url": "/static/js/runtime-main.e03af4ce.js"
+  },
+  {
+    "revision": "82599e53a4d3864ded45da25e3a131fb",
+    "url": "/static/media/uci.82599e53.png"
+  },
+  {
+    "revision": "5f56d92b5e805c96620dd575992f6667",
+    "url": "/static/media/veeral_bhagat.5f56d92b.pdf"
+  }
+]);
